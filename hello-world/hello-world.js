7470b957c495dd571da5ed67439c11b13089984e
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-06 12:25:50 -0400

    Init with some basics

---
+++ hello-world/hello-world.js
@@ -0,0 +1,5 @@
+var hello = function (string) {
+    console.log('Hello' + string);
+};
+hello('john');
+hello(32);
---
+++ hello-world/hello-world.ts
@@ -0,0 +1,5 @@
+const hello = (string : String) =>{
+  console.log('Hello' + string)
+}
+
+hello('john')
